8db209b2db9c2e9d037f45588b0bffe062bd4f21
Joshua Piper <joshua.g.piper@gmail.com>
2020-09-21 11:13:26 +1000

    created recordTransaction function to save a transaction to the db.

--- public/db.js
+++ public/db.js
@@ -1,1 +1,13 @@
-const request = window.indexedDB.open("budgetDatabase", 1);
+let db;
+
+const request = indexedDB.open("budgetDB", 1);
+
+request.onupgradeneeded = event => {
+    const db = event.target.result;
+
+    const budgetStore = db.createObjectStore("pending", { autoIncrement: true })
+};
+
+request.onerror = function (event) {
+    console.log("You dun goofed. Error code:" + event.target.errorCode);
+};
