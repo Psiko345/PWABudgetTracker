add31af38982f902938004e7737690015999d440
Joshua Piper <joshua.g.piper@gmail.com>
2020-09-21 11:40:03 +1000

    completed code for adding records to the database, both on/offline.

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 let db;
 
 const request = indexedDB.open("budgetDB", 1);
@@ -9,5 +11,52 @@ request.onupgradeneeded = event => {
 };
 
 request.onerror = function (event) {
-    console.log("You dun goofed. Error code:" + event.target.errorCode);
+    console.log("Something goofed. Error code:" + event.target.errorCode);
 };
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+function recordTransaction(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+
+                    const store = transaction.objectStore("pending");
+
+                    store.clear();
+                })
+        }
+    }
+}
+
+window.addEventListener("online", checkDatabase);
